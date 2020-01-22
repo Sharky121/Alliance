383e6a774d277f5f0b504db659b9fba93526fe13
Sharky121 <Sharky121@mail.ru>
2020-01-22 23:14:13 +0200

    source: drob

--- parts/init.php
+++ parts/init.php
@@ -3,11 +3,12 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     $link = @mysqli_connect(
         'localhost',
         'root',
         '123456',
-        'plastprom'
+        'alliance'
     ) or die('Ошибка: Невозможно подключиться к MySQL '. mysqli_connect_error());
 
     @mysqli_set_charset($link, "utf8") or die('mysqli: невозможно устновить кодировку '. mysqli_connect_error());
