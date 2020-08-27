eab42bcbe689591d8ec440112c20b8b32e66844d
Sharky <Sharky121@mail.ru>
2020-08-27 13:12:39 +0300

    fix: поправляет подключение к базе

--- parts/init.php
+++ parts/init.php
@@ -9,14 +9,14 @@
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
     $link = @mysqli_connect(
-      'localhost',
-      'root',
-      '12345678',
-      'alliance'
-//        'p530117.mysql.ihc.ru',
-//        'p530117_alliance',
-//        'frutWYeStC',
-//        'p530117_alliance'
+//      'localhost',
+//      'root',
+//      '12345678',
+//      'alliance'
+        'p530117.mysql.ihc.ru',
+        'p530117_alliance',
+        'frutWYeStC',
+        'p530117_alliance'
     ) or die('Ошибка: Невозможно подключиться к MySQL '. mysqli_connect_error());
 
     @mysqli_set_charset($link, "utf8") or die('mysqli: невозможно устновить кодировку '. mysqli_connect_error());
